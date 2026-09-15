91c5d114f86e1222043204071566efa86b8b2382
fkeisyaghifari <fkeisyaghifari@gmail.com>
2026-09-15 09:06:47 +0700

    reverse

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// this is used to render the app in the root element of the HTML file
-// again this is a new way of rendering the app in React 18, using createRoot instead of ReactDOM.render
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
